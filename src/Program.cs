9ca07f2543328a223c464ef61425d7fb9cc85149
Erik Davidson <erik@erikd.org>
2010-02-01 02:48:28 -0600

    Upgraded project files to VS2008

--- src/Program.cs
+++ src/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Text;
 using LibMinecraft;
